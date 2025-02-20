cdd2846f24dfaf322fdced3eb1e2e586e9f851e0
Paul Overy <titmouse001@gmail.com>
2025-02-20 20:25:31 +0000

    rename

--- asm/Build.bat
+++ asm/Build.bat
@@ -1,12 +1,12 @@
-:: outputs: "cybernoid-tap.tap" & "cybernoid-tap.lst"
-:: note: "cybernoid-tap.lst" is a nice to have with extra details like clock cycles []
+:: outputs: "cybernoid.tap" & "cybernoid.lst"
+:: note: "cybernoid.lst" is a nice to have with extra details like clock cycles []
 
 @if exist cybernoid-tap.tap del cybernoid-tap.tap
-zasm-tool\zasm.exe --z80 --opcodes --labels --cycles  cybernoid-tap.asm
+zasm-tool\zasm.exe --z80 --opcodes --labels --cycles  cybernoid.asm
 
 :: No making any changes yet - make sure the reworking anything 
 :: hardcoded is still the same binary is still the same.
-fc /b cybernoid-tap.tap RefNoLabels/cybernoid-tap.tap
+fc /b cybernoid.tap RefNoLabels/cybernoid-tap.tap
 
 pause
 
